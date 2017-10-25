c810d21b994a792a3a5e4247f29c10bacb9ec2bc
Pascal Weisenburger <pascal.weisenburger@web.de>
2017-10-25 11:57:00 +0200

    shortened proof for aggregation lemma

--- ReTierProofAggregation.v
+++ ReTierProofAggregation.v
@@ -1,9 +1,8 @@
 Require Import ReTierSyntax.
 Require Import ReTierStaticSemantics.
 Require Import ReTierDynamicSemantics.
-Require Coq.Lists.ListSet.
 
-Theorem Aggregation_Multiple : forall
+Lemma Aggregation_Multiple : forall
     (c: context)
     (p0 p1 : P)
     (peers: Coq.Lists.ListSet.set p)
@@ -13,20 +12,18 @@ Theorem Aggregation_Multiple : forall
   getTieMult c (p0, p1) = Some multiple ->
   has_type c value value_type ->
   Phi (getTies c) p0 p1 peers value value_type = Some term ->
-    has_type c term (List value_type).
+  has_type c term (List value_type).
 Proof.
 intros c p0 p1 peers term value value_type.
 intros H_multiple H_value_type H_Phi_term.
 unfold getTieMult in H_multiple.
 generalize dependent term.
-induction peers as [| peer0 peers IH_peers ].
-- intros term H_Phi_term.
-  unfold Phi in H_Phi_term.
+induction peers as [| peer0 peers IH_peers ]; intros term H_Phi_term.
+- simpl in H_Phi_term.
   rewrite H_multiple in H_Phi_term.
   inversion H_Phi_term.
   apply T_Nil.
-- intros term H_Phi_term.
-  assert (H : exists term, Phi (getTies c) p0 p1 peers value value_type = Some term).
+- assert (H : exists term, Phi (getTies c) p0 p1 peers value value_type = Some term).
   + clear IH_peers H_Phi_term.
     induction peers as [| peer1 peers IH_peers ].
     * simpl.
@@ -40,20 +37,19 @@ induction peers as [| peer0 peers IH_peers ].
       exists (cons value term2).
       reflexivity.
   + inversion H as [ term2 H_Phi_term2 ].
-    unfold Phi in IH_peers, H_Phi_term, H_Phi_term2.
+    simpl in IH_peers, H_Phi_term, H_Phi_term2.
     rewrite H_multiple in H_Phi_term.
     rewrite H_Phi_term2 in H_Phi_term.
     rewrite H_Phi_term2 in IH_peers.
     clear H_Phi_term2.
     inversion H_Phi_term.
     apply T_Cons.
-    * exact H_value_type.
+    * assumption.
     * apply IH_peers.
       reflexivity.
 Qed.
 
-
-Theorem Aggregation_Optional : forall
+Lemma Aggregation_Optional : forall
     (c: context)
     (p0 p1 : P)
     (peers: Coq.Lists.ListSet.set p)
@@ -63,27 +59,27 @@ Theorem Aggregation_Optional : forall
   getTieMult c (p0, p1) = Some optional ->
   has_type c value value_type ->
   Phi (getTies c) p0 p1 peers value value_type = Some term ->
-    has_type c term (Option value_type).
+  has_type c term (Option value_type).
 Proof.
 intros c p0 p1 peers term value value_type.
 intros H_multiple H_value_type H_Phi_term.
 unfold getTieMult in H_multiple.
 destruct peers.
-- unfold Phi in H_Phi_term.
+- simpl in H_Phi_term.
   rewrite H_multiple in H_Phi_term.
   inversion H_Phi_term.
   apply T_None.
-- unfold Phi in H_Phi_term.
+- simpl in H_Phi_term.
   rewrite H_multiple in H_Phi_term.
   destruct peers.
   + inversion H_Phi_term.
     apply T_Some.
-    exact H_value_type.
+    assumption.
   + contradict H_Phi_term.
     congruence.
 Qed.
 
-Theorem Aggregation_Single : forall
+Lemma Aggregation_Single : forall
     (c: context)
     (p0 p1 : P)
     (peers: Coq.Lists.ListSet.set p)
@@ -93,17 +89,17 @@ Theorem Aggregation_Single : forall
   getTieMult c (p0, p1) = Some single ->
   has_type c value value_type ->
   Phi (getTies c) p0 p1 peers value value_type = Some term ->
-    has_type c term value_type.
+  has_type c term value_type.
 Proof.
 intros c p0 p1 peers term value value_type.
 intros H_multiple H_value_type H_Phi_term.
 unfold getTieMult in H_multiple.
 destruct peers.
-- unfold Phi in H_Phi_term.
+- simpl in H_Phi_term.
   rewrite H_multiple in H_Phi_term.
   contradict H_Phi_term.
   congruence.
-- unfold Phi in H_Phi_term.
+- simpl in H_Phi_term.
   rewrite H_multiple in H_Phi_term.
   destruct peers.
   + inversion H_Phi_term.
@@ -112,7 +108,7 @@ destruct peers.
     congruence.
 Qed.
 
-Theorem Aggregation : forall
+Lemma Aggregation : forall
     (c: context)
     (p0 p1 : P)
     (peers: Coq.Lists.ListSet.set p)
@@ -131,30 +127,12 @@ intros c p0 p1 peers value value_type term term_type.
 intros H_tie0 H_tie1 H_Phi_term H_phi_term_type H_value_type.
 case_eq (getTieMult c (p0, p1)).
 - intros multiplicity H_tie.
-  destruct multiplicity.
-  + unfold phi in H_phi_term_type.
-    rewrite H_tie in H_phi_term_type.
-    inversion H_phi_term_type as [ H_term_type ].
-    apply Aggregation_Multiple with p0 p1 peers value.
-    * exact H_tie.
-    * exact H_value_type.
-    * exact H_Phi_term.
-  + unfold phi in H_phi_term_type.
-    rewrite H_tie in H_phi_term_type.
-    inversion H_phi_term_type as [ H_term_type ].
-    apply Aggregation_Optional with p0 p1 peers value.
-    * exact H_tie.
-    * exact H_value_type.
-    * exact H_Phi_term.
-  + unfold phi in H_phi_term_type.
-    rewrite H_tie in H_phi_term_type.
-    inversion H_phi_term_type as [ H_term_type ].
-    rewrite <- H_term_type.
-    apply Aggregation_Single with p0 p1 peers value.
-    * exact H_tie.
-    * exact H_value_type.
-    * exact H_Phi_term.
-  + contradiction.
+  unfold phi in H_phi_term_type.
+  rewrite H_tie in H_phi_term_type.
+  destruct multiplicity; inversion H_phi_term_type as [ H_term_type ]; subst.
+  + eapply Aggregation_Multiple; eassumption.
+  + eapply Aggregation_Optional; eassumption.
+  + eapply Aggregation_Single; eassumption.
 - contradiction.
 Qed.
 
